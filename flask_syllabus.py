b9e410ad2006451bd1695a347f658b67ff94b22e
Michal Young <michal.young@gmail.com>
2016-08-10 14:05:39 -0700

    Always make globally accessible

--- flask_syllabus.py
+++ flask_syllabus.py
@@ -25,14 +25,7 @@ import pre  # Preprocess schedule file
 # Globals
 ###
 app = flask.Flask(__name__)
-schedule = "static/schedule.txt"  # This should be configurable
 import CONFIG
-
-
-import uuid
-app.secret_key = str(uuid.uuid4())
-app.debug=CONFIG.DEBUG
-app.logger.setLevel(logging.DEBUG)
 
 
 ###
@@ -87,12 +80,8 @@ if __name__ == "__main__":
     app.secret_key = str(uuid.uuid4())
     app.debug=CONFIG.DEBUG
     app.logger.setLevel(logging.DEBUG)
-    if app.debug: 
-        print("Accessible only on localhost")
-        app.run(port=CONFIG.PORT)  # Accessible only on localhost
-    else:
-        print("Opening for global access on port {}".format(CONFIG.PORT))
-        app.run(port=CONFIG.PORT, host="0.0.0.0")
+    print("Opening for global access on port {}".format(CONFIG.PORT))
+    app.run(port=CONFIG.PORT, host="0.0.0.0")
 else:
     # Running from cgi-bin or from gunicorn WSGI server, 
     # which makes the call to app.run.  Gunicorn may invoke more than
